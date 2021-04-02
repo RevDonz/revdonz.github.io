c0b793745ec198a7666fbd22670cc9662bb9fbf8
RevDonz <aprilio842@gmail.com>
2021-04-02 16:02:12 +0700

    update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <div>
-            
+        <div className="bg-dark-main font-poppins">
+            <div className="flex justify-between items-center text-white w-4/5 mx-auto py-3">
+                <span className="text-xl">Reva Doni Aprilio</span>
+                <div className="flex space-x-3">
+                    <span>About Me</span>
+                    <span>Project</span>
+                </div>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'dark-main': '#263859',
+        'dark-second': '#242526',
+        'dark-third': '#3A3B3C',
+        'dark-txt': '#B8BBBF'
+      },
+    },
   },
   variants: {
     extend: {},
