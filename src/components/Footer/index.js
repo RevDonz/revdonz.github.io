e806c259282371b4f17ab89a049ea5f302d8d649
RevDonz <aprilio842@gmail.com>
2021-04-16 10:23:26 +0700

    ubah footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import { FaHeart, FaRegCopyright } from 'react-icons/fa'
 export default function index() {
     return (
         <div className="bg-dark-main font-poppins h-full">
-            <div className="container mx-auto px-5 lg:px-20 h-full w-full">
+            <div className="container mx-auto px-5 lg:px-20 h-full w-full lg:text-base text-sm">
                 <div className="text-white pt-5 items-center justify-center flex flex-row">
                     <span className="mr-2">Made with React JS, Tailwind CSS and</span>
                     <FaHeart />
